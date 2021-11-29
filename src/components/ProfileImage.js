c20944293a193bb08bd3c813b1899f7b7f9c005f
juliajulia89 <julkajanicka@gmail.com>
2021-11-29 18:04:25 +0100

    done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,9 @@
+const ProfileImage = ({ img }) => {
+  return (
+    <div>
+      <img src={img} className="profile" alt="profile" />
+    </div>
+  );
+};
+
+export default ProfileImage;
---
+++ src/components/Timestamp.js
@@ -0,0 +1,4 @@
+const TimeStamp = ({time}) => {
+  return <span className="timestamp">{time}</span>;
+};
+export default TimeStamp;
